eecfcced409c35ccdec4ca6fa674e22f1bbd0d50
Alex Young <alex@bytemark.co.uk>
2014-02-14 01:56:50 +0000

    Write the modified Gemfile.lock format

--- lib/justin/lock.rb
+++ lib/justin/lock.rb
@@ -6,14 +6,29 @@ module Justin
 
   module Lock
 
-    VERSION="0.0.1"
+    VERSION="0.0.2"
 
     def self.run
       dsl = Bundler::Dsl.new
-      dsl.eval_gemfile( "Gemfile", STDIN.read )
+      gemfile = STDIN.read
+      dsl.eval_gemfile( "Gemfile", gemfile )
       definition = dsl.to_definition( nil, true )
       definition.resolve_remotely!
+
       puts definition.to_lock
+
+      # Now we cheat like hell.  We need something to give us group
+      # information in the install step.  It just so happens that we
+      # can abuse the Gemfile.lock format *slightly* by tacking an
+      # extra section on the tail.  When justin-stall reads the
+      # Gemfile.lock, it chops this off before passing the two parts
+      # to the bundler code.
+      #
+      # The alternative to this is completely changing the
+      # Gemfile.lock format, which isn't something I'm hugely keen on
+      # doing right now if it can be avoided.
+      puts "GEMFILE"
+      puts gemfile.gsub(/^/, "  ")
     end
   end
 
